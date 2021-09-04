84640198d32e7a282b92edb32a0077e79089afd2
Sexettin <72075007+sexettin78@users.noreply.github.com>
2021-09-05 01:59:19 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+<script>
+
+setTimeout(aaa,5000);
+
+function aaa() {
+
+//KOMUT BURAYA GELECEK YUKARIDAKİ 5000 YAZAN YER 5 SANİYE DEMEK ÖRNEĞİN 5 SANİYE SONRA MERHABA ŞEKLİNDE BİLDİRİM VERELİM.
+alert("MERHABA");
+
+//BU KADAR
+}
+
+</script>
